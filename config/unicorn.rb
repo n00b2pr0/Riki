e73283f8ec1cc33a68e1f9795edb3ec580539e48
Tim <thogg4@gmail.com>
2014-01-17 19:14:41 -0700

    use unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,25 @@
+if ENV['RAILS_ENV'] == 'development'
+  worker_processes 1
+else
+  worker_processes 4
+end
+
+timeout 30
+preload_app true
+
+before_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
+  end
+  sleep 1
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+    Rails.logger.info('Connected to ActiveRecord')
+  end
+end
+
+
