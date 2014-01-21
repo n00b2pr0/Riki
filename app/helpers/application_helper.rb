d819db091824675837332942c7b171095757bc70
Tim <thogg4@gmail.com>
2014-01-21 16:07:53 -0700

    make markdown more flexible

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,9 +7,10 @@ module ApplicationHelper
   end
 
   def markdown(text)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(
-        :hard_wrap => true, :filter_html => true, :safe_links_only => true),
-        :no_intraemphasis => true, :fenced_code_blocks => true, :autolink => true)
+    markdown = Redcarpet::Markdown.new(
+      Redcarpet::Render::HTML.new(:safe_links_only => true),
+      :fenced_code_blocks => true, :autolink => true
+    )
     markdown.render(text || '').html_safe
   end
 
